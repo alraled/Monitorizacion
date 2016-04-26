19a42b622b1a7794830709a9f73661e00bfc83a1
alvaro.ramiro <alraled@hotmail.es>
2016-04-26 13:52:42 +0000

    Monitorizacion

---
+++ Laboratorio Monitorizacion/Features/Feature1/Feature1.feature
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="516e4591-eb84-4284-abca-f8a6d05cca9d" description="SharePoint Add-in Feature" featureId="516e4591-eb84-4284-abca-f8a6d05cca9d" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Laboratorio Monitorizacion Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="236cb14b-e06c-4684-886b-5d5f761e1509" />
+    <projectItemReference itemId="b3e755d6-364b-482d-885c-9bc6cb265f20" />
+    <projectItemReference itemId="3629d182-b7db-4cee-922a-5267ab2dd37e" />
+    <projectItemReference itemId="adc025e9-3715-4062-aada-599b13a51aa1" />
+    <projectItemReference itemId="711e939f-cc74-4a27-b6ac-fb6f8c08cf93" />
+  </projectItems>
+</feature>
---
+++ Laboratorio Monitorizacion/Scripts/hearthscore.js
@@ -0,0 +1,34 @@
+﻿'use strict'
+
+
+    $('button').button();
+    var healthScore;
+    var polInterval;
+
+    var getHealthScore = function() {
+        $.ajax({
+            type: "HEAD",
+            url: "_layouts/15/blank.htm",
+            success: function(data, status, xhr) {
+                healthScore = xhr.getResponseHeader("X-SharePointHealthScore");
+                $('#healthPara').text("Server health score: " + healthScore);
+                $('#healthBar').progressbar("option", "value", parseInt(healthScore));
+            }
+        });
+    };
+
+    var startPolling = function(parameters) {
+        $('#healthBar').progressbar({
+            max: 10,
+            value: 0
+        });
+        getHealthScore();
+        polInterval = setInterval(getHealthScore, 5000);
+    };
+
+    var stopPolling = function() {
+        clearInterval(polInterval);
+        $('#healthPara').text("Click Start Polling to poll the server for health scores.");
+        $('#healthBar').progressbar("destroy");
+
+    };
